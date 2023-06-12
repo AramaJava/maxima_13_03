430eb9bfdafbad151415a2337978deea00af9a47
AramaJava <abr-aram@yandex.ru>
2023-06-13 02:26:01 +0500

    list

--- src/lessons/lesson14/Homework.java
+++ src/lessons/lesson14/Homework.java
@@ -6,7 +6,7 @@ public class Homework {
     public static void main(String[] args) {
 /*  1: Введи с клавиатуры 5 слов в список строк.
         Удали 3-ий элемент списка, и выведи оставшиеся элементы в обратном порядке.\
-
+*/
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> strings1 = new ArrayList<>();
         System.out.println("Введите 5 слов через пробел: ");
@@ -21,7 +21,7 @@ public class Homework {
     2: 1. Создай список из слов "мама", "мыла", "раму".
        2. После каждого слова вставь в список строку, содержащую слово "именно".
        3. Вывести результат на экран, каждый элемент списка с новой строки.
-
+*/
         List<String> strings2 = new ArrayList<>();
         strings2.add("мама");
         strings2.add("мыла");
@@ -33,7 +33,7 @@ public class Homework {
             }
         }
         System.out.println(strings2);
-        */
+
         /*
     3:
         1. Создай список слов, заполни его самостоятельно.
@@ -44,7 +44,7 @@ public class Homework {
         2.4. с другими словами ничего не делать.
 
       */
-     /*
+
         List<String> strings = new ArrayList<String>();
         strings.add("роза");
         strings.add("лоза");
@@ -54,60 +54,36 @@ public class Homework {
         for (String string : strings) {
             System.out.println(string);
         }
-    }
-
-    public static List<String> fix(List<String> strings) {
-
-        for (int i = 0; i < strings.size(); i++) {
-            if (strings.get(i).contains("р") && !strings.get(i).contains("л")) {
-                strings.remove(i);
-            }
-            if (strings.get(i).contains("л")) {
-                strings.set(i, strings.get(i).concat(strings.get(i)));
-            }
-        }
-        return strings;
-    }
-   */
-/*
+    /*
      4:   1. Введи с клавиатуры 10 слов в список строк.
           2. Метод doubleValues должен удваивать слова по принципу:
             "альфа","бета","гамма"->"альфа","альфа","бета","бета","гамма","гамма"
           3. Выведи результат на экран, каждое значение с новой строки.
-
-*//*
-        Scanner scanner = new Scanner(System.in);
+*/
+        //Scanner scanner = new Scanner(System.in);
         List<String> strings4 = new ArrayList<>();
         System.out.println("Введите 10 слов через пробел: ");
-        for (int i = 0; i < 10; i++) {
+        for (
+                int i = 0;
+                i < 10; i++) {
             strings4.add(scanner.next());
         }
 
         List<String> outList = doubleValues(strings4);
 
-        for (String s:outList
-             ) {
+        for (
+                String s : outList
+        ) {
             System.out.println(s);
         }
-    }
 
-    public static List<String> doubleValues(List<String> strings) {
-        for (int i = 0; i < strings.size(); i++) {
-            if (i % 2 == 0) {
-                strings.add(i, strings.get(i));
-            }
-        }
-
-        return strings;
-    }
-*/
         /*
       5:   1. Введи с клавиатуры 10 слов в список строк.
            2. Определить, является ли список упорядоченным по возрастанию длины строки.
            3. В случае отрицательного ответа вывести на экран индекс первого элемента,
            нарушающего такую упорядоченность .*/
 
-        Scanner scanner = new Scanner(System.in);
+        //    Scanner scanner = new Scanner(System.in);
         List<String> strings5 = new ArrayList<>();
         System.out.println("Введите 10 слов через пробел: ");
         for (int i = 0; i < 10; i++) {
@@ -125,6 +101,26 @@ public class Homework {
         }
         System.out.println("Признак упорядоченности списка: " + isSorted);
     }
+
+    public static List<String> fix(List<String> strings) {
+
+        for (int i = 0; i < strings.size(); i++) {
+            if (strings.get(i).contains("р") && !strings.get(i).contains("л")) {
+                strings.remove(i);
+            }
+            if (strings.get(i).contains("л")) {
+                strings.set(i, strings.get(i).concat(strings.get(i)));
+            }
+        }
+        return strings;
+    }
+
+    public static List<String> doubleValues(List<String> strings) {
+        for (int i = 0; i < strings.size(); i++) {
+            if (i % 2 == 0) {
+                strings.add(i, strings.get(i));
+            }
+        }
+        return strings;
+    }
 }
-
-
